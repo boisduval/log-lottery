f2290e30533f03d4f7fd0d4b9892a82be330fcda
boisduval <sniksil@126.com>
2025-02-17 11:17:01 +0800

    feat(router): switch to hash history mode
    - Replace createWebHistory with createWebHashHistory
    - Update router configuration to use hash-based routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Layout from "@/layout/index.vue";
 import Home from "@/views/Home/index.vue";
 export const configRoutes = {
@@ -132,7 +132,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
